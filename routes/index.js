bd5d1cad9063a03c18c5420a8f41f186f272464a
DarianNwankwo <dnwankwo404@gmail.com>
2018-10-09 02:37:15 -0400

    Added workout route

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,29 @@ var express           = require("express"),
     // fs                = require("fs"),
     TechnicallyFit    = require("../models/technicallyfit");
 
+
+let workouts = [
+  {
+    day: "Tuesday",
+    category: "Armertins",
+    exercise: [
+      // Name, Sets, Reps, Rest
+      ["Cable Front Raise", 4, 20, 30],
+      ["Dumbbell Rear Delt Row", 4, 20, 30],
+      ["Dumbbell Lateral Raises", 3, 25, 30],
+      ["Upright Cable Row", 4, 10, 30],
+      ["Dumbbell Shoulder Press", 5, 12, 30],
+      ["Tricep Pulldowns", 5, 12, 30],
+      ["Bicep Cable Curls", 5, 12, 30],
+      ["EZ Bar Skull Crushers", 4, 10, 30],
+      ["Reverse Curls", 4, 10, 30],
+      ["EZ Bar Curls", 5, 12, 30],
+      ["Tricep Dip (Weighted)", 5, 20, 30],
+      ["Preacher Curls", 5, 100, 30],
+    ]
+  },
+]
+
 /* GET home page. */
 router.get("/", function(req, res, next) {
   // res.render("index", { title: "Express" });
@@ -36,7 +59,7 @@ router.post("/", function(req, res) {
 });
 
 router.get("/workout", function(req, res) {
-  res.render("technicallyfit/workout");
+  res.render("technicallyfit/workout", { workouts: workouts });
 }); 
 
 
